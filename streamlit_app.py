bf5649894d823027d7f3159922c344689bcfb218
Reddy2001 <77780137+Reddy2001@users.noreply.github.com>
2024-03-28 12:18:35 +0530

    error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ ingredients_list=st.multiselect('choose up to 5 ingredients:',my_dataframe,max_s
 if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
-        ingredients_string+=x+' '
+        ingredients_string+=fruit_chosen+' '
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
